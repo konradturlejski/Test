88c52dd7189ee363d1707c92da093e3badeeea4a
konradturlejski <35076286+konradturlejski@users.noreply.github.com>
2018-12-29 13:56:16 +0100

    Add files via upload

---
+++ GenreButtonScreen.swift
@@ -0,0 +1,51 @@
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        sender.pulsate()
+        MPMediaLibrary.requestAuthorization { (status) in
+            
+            if status == .authorized {
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
+        
+        playGenre(genre: sender.currentTitle!)
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        sender.shake()
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        sender.flash()
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+        
+    }
+    
+    
+}
